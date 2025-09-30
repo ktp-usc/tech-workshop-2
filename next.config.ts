977bb87c2091b5f4e2f8f93a985773a87413bb89
bguliano <72895782+bguliano@users.noreply.github.com>
2025-09-29 21:16:44 -0400

    setup for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const repo = "tech-workshop-2";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}`
 };
 
 export default nextConfig;
